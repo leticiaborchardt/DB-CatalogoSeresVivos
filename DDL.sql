5c9131b93ee7337bbe867ed465ba3fd05d85acdc
Letícia Borchardt <83610072+leticiaborchardt@users.noreply.github.com>
2024-07-31 13:50:43 -0300

    Atualizações no DDL - constraints e modificações nas tabelas relacionadas a localizações

--- DDL.sql
+++ DDL.sql
@@ -1,105 +1,126 @@
 -- Tabela Reino
-CREATE TABLE Reino (
+CREATE TABLE reino (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
 
 -- Tabela Filo
-CREATE TABLE Filo (
+CREATE TABLE filo (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    reino_id INTEGER REFERENCES Reino(id)
+    reino_id INTEGER NOT NULL REFERENCES reino(id)
 );
 
 -- Tabela Classe
-CREATE TABLE Classe (
+CREATE TABLE classe (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    filo_id INTEGER REFERENCES Filo(id)
+    filo_id INTEGER NOT NULL REFERENCES filo(id)
 );
 
 -- Tabela Ordem
-CREATE TABLE Ordem (
+CREATE TABLE ordem (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    classe_id INTEGER REFERENCES Classe(id)
+    classe_id INTEGER NOT NULL REFERENCES classe(id)
 );
 
 -- Tabela Família
-CREATE TABLE Familia (
+CREATE TABLE familia (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    ordem_id INTEGER REFERENCES Ordem(id)
+    ordem_id INTEGER NOT NULL REFERENCES ordem(id)
 );
 
 -- Tabela Gênero
-CREATE TABLE Genero (
+CREATE TABLE genero (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    familia_id INTEGER REFERENCES Familia(id)
+    familia_id INTEGER NOT NULL REFERENCES familia(id)
 );
 
 -- Tabela Espécie
-CREATE TABLE Especie (
+CREATE TABLE especie (
     id SERIAL PRIMARY KEY,
     nome_cientifico VARCHAR(150) NOT NULL,
-    nome_comum VARCHAR(150),
+    nome_comum VARCHAR(150) NOT NULL,
     descricao TEXT,
-    id_genero INTEGER REFERENCES Genero(id),
+    id_genero INTEGER NOT NULL REFERENCES genero(id),
     status_conservacao VARCHAR(50),
-    ultima_observacao TIMESTAMP,
-    populacao_total INTEGER
+    data_ultima_observacao TIMESTAMP NOT NULL,
+    populacao_total INTEGER NOT NULL CHECK (populacao_total >= 0)
 );
 
--- Tabela Localizacao
-CREATE TABLE Localizacao (
+-- Tabela País
+CREATE TABLE pais(
     id SERIAL PRIMARY KEY,
-    regiao GEOMETRY NOT NULL,
-    nome VARCHAR(150) NOT NULL
+    nome VARCHAR(150) NOT NULL UNIQUE
 );
 
--- Tabela Area_Protegida
-CREATE TABLE Area_Protegida (
+-- Tabela Bioma
+CREATE TABLE bioma(
     id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL UNIQUE
+);
+
+-- Tabela Localização
+CREATE TABLE localizacao (
+    id SERIAL PRIMARY KEY,
+    regiao GEOGRAPHY NOT NULL,
     nome VARCHAR(150) NOT NULL,
-    id_localizacao INTEGER REFERENCES Localizacao(id)
+    id_pais INTEGER NOT NULL REFERENCES pais(id),
+    area_protegida BOOLEAN NOT NULL
 );
 
--- Tabela Especie_Area_Protegida
-CREATE TABLE Especie_Area_Protegida (
-	id SERIAL PRIMARY KEY,
-    id_especie INTEGER REFERENCES Especie(id),
-    id_area_protegida INTEGER REFERENCES Area_Protegida(id)
-);
-
--- Tabela Especie_Localizacao
-CREATE TABLE Especie_Localizacao (
-	id SERIAL PRIMARY KEY,
-    id_especie INTEGER REFERENCES Especie(id),
-    id_localizacao INTEGER REFERENCES Localizacao(id),
-    populacao_local INTEGER
-);
-
--- Tabela Doenca
-CREATE TABLE Doenca (
+-- Tabela Localização-Bioma
+CREATE TABLE localizacao_bioma (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    id_localizacao INTEGER NOT NULL REFERENCES localizacao(id),
+    id_bioma INTEGER NOT NULL REFERENCES bioma(id),
+    UNIQUE (id_localizacao, id_bioma)
+);
+
+-- Tabela Espécie-Localização
+CREATE TABLE especie_localizacao (
+    id SERIAL PRIMARY KEY,
+    id_especie INTEGER NOT NULL REFERENCES especie(id),
+    id_localizacao INTEGER NOT NULL REFERENCES localizacao(id),
+    populacao_local INTEGER NOT NULL CHECK (populacao_local >= 0),
+    UNIQUE (id_especie, id_localizacao)
+);
+
+-- Tabela Doença
+CREATE TABLE doenca (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE,
     descricao TEXT
 );
 
--- Tabela Especie_Doenca
-CREATE TABLE Especie_Doenca (
-	id SERIAL PRIMARY KEY,
-    id_especie INTEGER REFERENCES Especie(id),
-    id_doenca INTEGER REFERENCES Doenca(id),
-    taxa_mortalidade FLOAT
+-- Tabela Espécie-Doença
+CREATE TABLE especie_doenca (
+    id SERIAL PRIMARY KEY,
+    id_especie INTEGER NOT NULL REFERENCES especie(id),
+    id_doenca INTEGER NOT NULL REFERENCES doenca(id),
+    taxa_mortalidade FLOAT NOT NULL CHECK (taxa_mortalidade >= 0 AND taxa_mortalidade <= 1),
+    UNIQUE (id_especie, id_doenca)
 );
 
--- Tabela Interacao_Ecologica
-CREATE TABLE Interacao_Ecologica (
+-- Tabela de Interação Ecológica
+CREATE TABLE interacao_ecologica (
     id SERIAL PRIMARY KEY,
-    id_especie1 INTEGER REFERENCES Especie(id),
-    id_especie2 INTEGER REFERENCES Especie(id),
+    id_especie1 INTEGER NOT NULL REFERENCES especie(id),
+    id_especie2 INTEGER NOT NULL REFERENCES especie(id),
     tipo VARCHAR(100),
-    descricao TEXT
+    descricao TEXT,
+    CHECK (id_especie1 <> id_especie2),
+    UNIQUE (id_especie1, id_especie2, tipo)
+);
+
+-- Tabela Historico-Espécie
+CREATE TABLE historico_especie (
+    id SERIAL PRIMARY KEY,
+    id_especie INTEGER NOT NULL REFERENCES especie(id),
+    ultimo_status VARCHAR(50) NOT NULL,
+    ultima_populacao INTEGER NOT NULL CHECK (ultima_populacao >= 0),
+    data_hora TIMESTAMP NOT NULL
 );
