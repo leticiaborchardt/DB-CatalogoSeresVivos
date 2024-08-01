e7bc085f545b339c780926096d06f9a95249a7fe
Leticia <leticiaborchardt123@gmail.com>
2024-08-01 18:07:39 -0300

    Adicionada trigger para atualizar população total da espécie com base na população local de suas localizações registradas

--- TriggerView.sql
+++ TriggerView.sql
@@ -19,3 +19,24 @@ CREATE TRIGGER trg_update_data_ultima_observacao
 AFTER INSERT ON historico_especie
 FOR EACH ROW
 EXECUTE FUNCTION update_data_ultima_observacao();
+
+-- Trigger responsável por atualizar o campo populacao_total na tabela especie conforme populacao_local adicionada nas localizações da espécie.
+CREATE OR REPLACE FUNCTION atualizar_populacao_total()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE especie
+    SET populacao_total = (
+        SELECT COALESCE(SUM(populacao_local), 0)
+        FROM especie_localizacao
+        WHERE id_especie = NEW.id_especie
+    )
+    WHERE id = NEW.id_especie;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_atualizar_populacao_total
+AFTER INSERT OR UPDATE ON especie_localizacao
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_populacao_total();
